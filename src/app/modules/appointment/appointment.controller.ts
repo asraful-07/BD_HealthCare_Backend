4832beda398ddc2dc2f7fb4854954827666d7c5f
asraful-07 <asraful0508@gmail.com>
2026-02-17 17:51:11 +0600

    appointment api done

--- src/app/modules/appointment/appointment.controller.ts
+++ src/app/modules/appointment/appointment.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import { catchAsync } from "../../shared/catchAsync";
-import { BookAppointmentService } from "./appointment.service";
+import {
+  BookAppointmentService,
+  BookAppointmentWithPayLaterService,
+  InitiatePaymentService,
+} from "./appointment.service";
 import { sendResponse } from "../../shared/sendResponse";
 import status from "http-status";
 
@@ -14,6 +18,38 @@ export const BookAppointmentController = catchAsync(
       success: true,
       message: "Appointment booked successfully",
       data: result,
+    });
+  },
+);
+
+export const BookAppointmentWithPayLaterController = catchAsync(
+  async (req: Request, res: Response) => {
+    const payload = req.body;
+    const user = req.user;
+    const appointment = await BookAppointmentWithPayLaterService(payload, user);
+    sendResponse(res, {
+      success: true,
+      httpStatusCode: status.CREATED,
+      message: "Appointment booked successfully with Pay Later option",
+      data: appointment,
+    });
+  },
+);
+
+export const InitiatePaymentController = catchAsync(
+  async (req: Request, res: Response) => {
+    const appointmentId = req.params.id;
+    const user = req.user;
+    const paymentInfo = await InitiatePaymentService(
+      appointmentId as string,
+      user,
+    );
+
+    sendResponse(res, {
+      success: true,
+      httpStatusCode: status.OK,
+      message: "Payment initiated successfully",
+      data: paymentInfo,
     });
   },
 );
--- src/app/modules/appointment/appointment.routes.ts
+++ src/app/modules/appointment/appointment.routes.ts
@@ -1,13 +1,28 @@
 import { Router } from "express";
-import { BookAppointmentController } from "./appointment.controller";
+import {
+  BookAppointmentController,
+  BookAppointmentWithPayLaterController,
+  InitiatePaymentController,
+} from "./appointment.controller";
 import { checkAuth } from "../../middleware/checkAuth";
 import { Roles } from "../../../generated/prisma/enums";
 
 const appointmentRoutes = Router();
+
 appointmentRoutes.post(
   "/book",
   checkAuth(Roles.PATIENT),
   BookAppointmentController,
+);
+appointmentRoutes.post(
+  "/book-appointment-with-pay-later",
+  checkAuth(Roles.PATIENT),
+  BookAppointmentWithPayLaterController,
+);
+appointmentRoutes.post(
+  "/initiate-payment/:id",
+  checkAuth(Roles.PATIENT),
+  InitiatePaymentController,
 );
 
 export default appointmentRoutes;
