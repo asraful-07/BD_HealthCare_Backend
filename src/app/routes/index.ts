d298b2e85f56993ed7e886f92d37463ffe574ea6
asraful-07 <asraful0508@gmail.com>
2026-02-17 17:52:25 +0600

    add super admin routes

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,9 @@ import authRoutes from "../modules/auth/auth.routes";
 import userRoutes from "../modules/user/user.routes";
 import doctorRoutes from "../modules/doctor/doctor.routes";
 import adminRoutes from "../modules/admin/admin.routes";
+import scheduleRoutes from "../modules/schedule/schedule.routes";
+import doctorScheduleRoutes from "../modules/doctorSchedule/doctorSchedule.routes";
+import appointmentRoutes from "../modules/appointment/appointment.routes";
 
 const router = Router();
 
@@ -11,6 +14,9 @@ router.use("/specialty", specialtyRotes);
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/doctor", doctorRoutes);
-router.use("admin", adminRoutes);
+router.use("/admin", adminRoutes);
+router.use("/schedules", scheduleRoutes);
+router.use("/doctor-schedule", doctorScheduleRoutes);
+router.use("/appointment", appointmentRoutes);
 
 export const IndexRoutes = router;
