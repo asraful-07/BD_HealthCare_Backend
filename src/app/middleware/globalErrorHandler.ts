d8975583273365d2afed983063cc543a8183deba
asraful-07 <asraful0508@gmail.com>
2026-02-19 16:35:11 +0600

    global error handler done

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -8,6 +8,7 @@ import { handleZodError } from "../errorHelper/handleZodError";
 import AppError from "../errorHelper/AppError";
 import z from "zod";
 import { deleteFileFromCloudinary } from "../config/cloudinary.config";
+import { deleteUploadedFilesFromGlobalErrorHandler } from "../utils/deleteUploadedFilesFromGlobalErrorHandler";
 
 export const globalErrorHandler = async (
   err: any,
@@ -19,15 +20,19 @@ export const globalErrorHandler = async (
     console.log("Error from Global Error Handler", err);
   }
 
-  //* global cloudinary image deleted
-  if (req.file) {
-    await deleteFileFromCloudinary(req.file.path);
-  }
+  //todo global cloudinary image deleted
+  //* first cloudinary so use this code
+  // if (req.file) {
+  //   await deleteFileFromCloudinary(req.file.path);
+  // }
 
-  if (req.files && Array.isArray(req.files) && req.files.length > 0) {
-    const imageUrls = req.files.map((file) => file.path);
-    await Promise.all(imageUrls.map((url) => deleteFileFromCloudinary(url)));
-  }
+  // if (req.files && Array.isArray(req.files) && req.files.length > 0) {
+  //   const imageUrls = req.files.map((file) => file.path);
+  //   await Promise.all(imageUrls.map((url) => deleteFileFromCloudinary(url)));
+  // }
+
+  //* than you senior so delete multiple image delete on cloudinary and db
+  await deleteUploadedFilesFromGlobalErrorHandler(req);
   //* ---
 
   let errorSources: TErrorSources[] = [];
